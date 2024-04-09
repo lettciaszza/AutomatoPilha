ade1f4efa8bc5888c9cbea1e7e8f9f8afa48f834
Letícia  Souza <72477802+lettciaszza@users.noreply.github.com>
2024-04-09 12:44:02 -0300

    Upate Automato Pilha

---
+++ Comentário.java
@@ -0,0 +1,8 @@
+
+
+//OBJETIVO:
+
+//- Implementar um sistema que	reconheça uma linguagem  que reconheça 
+// a linguagem livre de contexto,  sendo implementar um Autômato com Pilha.
+
+// A implementação foi utilizada matrizes, sendo assim, não listas. E utilizei parte dos codigos disponivel no sigaa.
